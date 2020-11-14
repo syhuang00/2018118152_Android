7df8a5365d8cb17e1ea1caf5da1b431c641079d1
syhuang00 <70836639+syhuang00@users.noreply.github.com>
2020-11-14 20:40:37 +0800

    实验四 IntentTest实验代码

---
+++ 实验四 Intent应用/Intent应用IntentTest/IntentTest/app/src/main/java/com/example/intenttest/FirstActivity.java
@@ -0,0 +1,26 @@
+package com.example.intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
+        Button button1=(Button) findViewById(R.id.button_1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent("com.example.intenttest.ACTION_START");
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ 实验四 Intent应用/Intent应用IntentTest/IntentTest/app/src/main/java/com/example/intenttest/SecondActivity.java
@@ -0,0 +1,27 @@
+package com.example.intenttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+        Button button2=(Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+                startActivity(intent);
+            }
+        });
+    }
+}
