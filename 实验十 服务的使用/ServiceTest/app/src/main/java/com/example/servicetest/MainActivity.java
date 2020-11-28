dea94a88b6fcad915290fb9d59e28c8d67a6efc7
syhuang00 <70836639+syhuang00@users.noreply.github.com>
2020-11-28 16:41:42 +0800

    实验十 实验代码

---
+++ 实验十 服务的使用/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.servicetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startService=(Button) findViewById(R.id.start_service);
+        Button stopService=(Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+        Button startIntentService=(Button) findViewById(R.id.start_intent_service);
+        startIntentService.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.start_service:
+                Intent startIntent=new Intent(this,MyService.class);
+                startService(startIntent);   //启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent=new Intent(this,MyService.class);
+                stopService(stopIntent);   //停止服务
+                break;
+            case R.id.start_intent_service:
+                Log.d("MainActivity","Thread id is " + Thread.currentThread().getId());
+                Intent intentService=new Intent(this,MyIntentService.class);
+                startService(intentService);
+                break;
+            default:
+                break;
+        }
+    }
+}
